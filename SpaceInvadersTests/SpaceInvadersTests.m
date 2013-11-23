9da61791c2aa8f0cffcd791548848ce544a57dae
Attila Csala <attilacsala@gmail.com>
2013-11-23 14:15:55 +0100

    Initial Commit

---
+++ SpaceInvadersTests/SpaceInvadersTests.m
@@ -0,0 +1,34 @@
+//
+//  SpaceInvadersTests.m
+//  SpaceInvadersTests
+//
+//  Created by Attila Csala on 11/23/13.
+//  Copyright (c) 2013 Attila Csala. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpaceInvadersTests : XCTestCase
+
+@end
+
+@implementation SpaceInvadersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
